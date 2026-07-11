45bcce2fc5704a31285cf63458839b3c44218d68
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-07-11 14:27:14 +0530

    Time: 4 ms (42.54%), Space: 55.1 MB (62.99%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
 
-        unordered_map<int, int> mp;
+       int n = nums.size();
+       
+       for(int i =0; i<n;i++){
 
-        // Store all positive numbers
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0)
-                mp[nums[i]]++;
+        // for loop , check each element and swap them in their correct position
+        while(nums[i]>0 && nums[i]<n && nums[i] != nums[nums[i]-1]){
+            // ignore negative , check no is present inside the range , check whether number is already present in its correct position
+
+            swap(nums[i],nums[nums[i]-1]);
         }
+       }
 
-        // Find first missing positive
-        for (int i = 1; i <= nums.size(); i++) {
-            if (mp.find(i) == mp.end())
-                return i;
-        }
+       for(int i =0 ; i< n ; i ++){
+        // find the first incorrect postion 
+        if(nums[i] != i+1)
+            return i+1;       
+       }
+        
 
-        return nums.size() + 1;
+        return n + 1;
     }
 };
