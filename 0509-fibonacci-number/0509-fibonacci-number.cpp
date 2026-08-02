74033131eec4c34abea59a78eb5d153622ce66af
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-02 14:59:25 +0530

    Time: 14 ms (22.52%), Space: 7.8 MB (38.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)
-            return n ;
-        // create a dp graph    
-        vector<int> dp(n+1);
+        if(n==0)
+            return 0 ;
+        if(n==1)
+            return 1;
 
-        // dp 0 and dp 1 is 0 and 1 , fibonnaci rule 
-        dp[0]= 0;
-        dp[1]= 1;
-        for(int i =2; i<=n; i++){
-            // fib[n] = fib [n-1]+ fib [n-2] sum of ;ast two nos
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        // function inside function 1
+        int ans1 = fib(n-1);
+
+        // function inside function 2
+        int ans2 = fib(n-2);
+
+
+        return ans1 + ans2 ; 
+
         
     }
 };
