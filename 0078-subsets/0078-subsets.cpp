b1d07ce6e820794d5cd63cafb1d80afc70d23ac1
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-05 14:05:55 +0530

    Time: 3 ms (27.16%), Space: 12.8 MB (20.81%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    void soln(vector<int>& nums , int idx , int n , vector<int>& tmp , vector<vector<int>>& res){
+        // BASE CASE
+        if (idx==n){
+            res.push_back(tmp);
+            return ;
+        }
+        // 1ST CHOICE
+        soln(nums,idx+1,n,tmp,res);
+
+        // 2ND CHOICE
+        tmp.push_back(nums[idx]);
+        soln(nums,idx+1,n,tmp,res);
+        tmp.pop_back();
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> tmp;
+        vector<vector<int>> res; ;
+        soln(nums,0,n,tmp,res);
+        return res;
+        
+        
+    }
+};
