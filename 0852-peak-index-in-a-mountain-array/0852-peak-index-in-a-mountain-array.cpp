aba2e00ae46ec346e3f80121e181d37bed8bbd44
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-16 04:35:33 +0530

    Time: 0 ms (100%), Space: 63.5 MB (79.7%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0;
+        int high = arr.size()-1;
+        int res = 0;
+
+        while(low<high){
+            int guess = (low+high)/2;
+
+            if(arr[guess]<arr[guess+1])
+                low = guess + 1;
+            else{
+                res = guess;
+                high = guess;
+            }
+        }
+
+        return res;
+        
+    }
+};
