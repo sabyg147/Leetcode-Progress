bbcc2e1b2e042613158682c3e74a74a1bd1dd54b
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-17 23:14:02 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.74%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int res = 0;
+
+        while(low<=high){
+            int guess = (low+high)/2;
+
+            if(nums[guess]==target)
+                return guess;
+
+
+            //PART 2 : GUESS ELEMENT IS IN THE  ROTATED PART (K)
+            else if(nums[guess]>nums[n-1]){
+
+                    // IF TARGET IS GREATER THAN THE GUESS NUMBER , THE GREATER NUMBERS ARE ONLY PRESENT IN THE RIGHT SIDE OF THE GUESS
+                    if(nums[guess]<target)
+                        low = guess + 1;
+                    
+                    else if(nums[guess]> target){
+
+                        if(nums[0]>target){ // TARGET LIES IN THE 1ST PART , N-K PART SO WE NEED TO CHECK RIGHT PART 
+                            low = guess + 1;
+
+                        } 
+                        else  // TARGET LIES IN THE 2ND PART ONLY , CHECK LEFT PART 
+                            high = guess -1;
+
+                    }
+
+            }
+            // PART 3: GUESS ELEMENT LIES IN THE 1ST PART , THE NON ROTATED PART N-K
+            else{
+                    if(nums[guess]>target)
+                        high = guess - 1;
+                    else if(nums[guess]<target){
+                        //GREATER ELEMENT CAN LIE ON BOTH RIGHT AND LEFT
+
+                        if(nums[n-1]>=target){
+                            // Target lies in the part 1 , so move right 
+                            low = guess + 1;
+                
+                        }
+                        else
+                            high = guess - 1;
+                    }
+            }
+        }
+
+        return -1;
+        
+    }
+};
