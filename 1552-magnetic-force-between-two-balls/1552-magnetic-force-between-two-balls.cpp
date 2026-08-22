b72e44c8e2f4cd69b6958f709acb472259b869f9
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-22 20:55:30 +0530

    Time: 48 ms (58.37%), Space: 61.7 MB (38.06%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int fun(vector<int>& pos, int m , int guess){
+        int balls = 1;
+        int posn = pos[0];
+        for(int i=0;i<pos.size();i++){
+            int distance = pos[i]- posn;
+            if(distance<guess)
+                continue;
+            else{
+                balls++;
+                posn = pos[i];
+            }
+
+        }
+
+        if(balls>=m)
+            return true;
+        else
+            return false;
+    }
+    int maxDistance(vector<int>& pos, int m) {
+        int n = pos.size();
+        sort(pos.begin(),pos.end());
+        int low = 1;
+        int high = pos[n-1] - pos[0];
+        int res = 0;
+
+        while(low<=high){
+            int guess =(low+high)/2;
+
+            if(fun(pos,m,guess)){
+                res= guess;
+                low = guess +1;
+            }
+            else
+                high = guess - 1;
+        }
+
+        return res;
+    }
+};
