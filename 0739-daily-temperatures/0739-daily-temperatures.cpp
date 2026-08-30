8dc2ee041087df0a3ef08f154d5aa656d196627f
Sabyasachi Ghosh <sabyasachig147@gmail.com>
2026-08-30 13:18:10 +0530

    Time: 16 ms (80.73%), Space: 102.9 MB (63.05%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        vector<int> res(n,0);
+        stack<int> st;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && temp[st.top()]<=temp[i]){
+                st.pop();
+        }
+
+        if(st.empty())
+            res[i]=0;
+
+        else
+            res[i]=st.top()-i;
+
+        st.push(i);
+    }
+        
+    return res;
+    }
+};
